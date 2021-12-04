720987af29b4413c17c579956c7d6fea7504e258
intervisionlord <ads-net@mail.ru>
2021-12-05 00:20:17 +0300

    [0.2.2] Локализация, сборка, GUI
     * Добавлена поддержка локализации
     * Добавлена заготовка под GUI
     * Обновлен конфиг
     * Добавлены инстрнукции для сборщика WIN версий
     * Заготовка релизного README
     * Убран старый SH файл

--- CMS.py
+++ CMS.py
@@ -1,24 +1,28 @@
-import yaml
+import yaml, gettext, locale
+from sys  import exit
 import functions as func
 from tkinter import *
 
 try:
     conffile = open('conf/main.yml', 'r')
 except:
-    errorcode = 'Конфиг не найден'
+    errorcode = 'Config was not found'
     exit(errorcode)
-
-config = yaml.full_load(conffile)
-conffile.close()
+finally:
+    config = yaml.full_load(conffile)
+    conffile.close()
 
 vers = config['core']['version']
+locale = config['settings']['locale']
+
+gettext.translation('CarMusicSorter', localedir='l10n', languages=[locale]).install()
 
 window = Tk()
-window.iconphoto(True, PhotoImage(file = 'data/assets/icons/main.png'))
+window.iconphoto(True, PhotoImage(file = 'data/assets/imgs/main.png'))
 window.geometry('400x200')
 window.title(f'Car Music Sorter v.: {vers}')
 
-main_label = Label(window, text = 'Выберите директорию, в которой необходимо произвести\nпреобразование').grid(column = 0, row = 0)
-go_button = Button(window, text = 'Начать!', command = func.on_click('go')).grid(column = 1, row = 0)
+main_label = Label(window, text = _('Choose directory where tracks were taken from')).grid(column = 0, row = 0)
+go_button = Button(window, text = _('Begin!'), command = func.on_click('go')).grid(column = 1, row = 0)
 
 window.mainloop()
