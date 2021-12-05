6b8c64d77724941c56c8ccca83a3d55ae787f08f
intervisionlord <ads-net@mail.ru>
2021-12-05 03:01:24 +0300

    [0.2.3] About
     * Функционал "О программе"
     * Дополнены строки локализации
     * Изменен вызов GUI всплывающего окна

--- CMS.py
+++ CMS.py
@@ -1,28 +1,44 @@
-import yaml, gettext, locale
+import yaml, gettext, locale, os
+import functions.functions as func
 from sys  import exit
-import functions as func
 from tkinter import *
+from tkinter import Menu, messagebox
 
+# Проверяем конфиг
 try:
     conffile = open('conf/main.yml', 'r')
 except:
-    errorcode = 'Config was not found'
-    exit(errorcode)
-finally:
-    config = yaml.full_load(conffile)
-    conffile.close()
+    exit(messagebox.showerror('ERROR', 'Config file not found'))
 
+config = yaml.full_load(conffile)
+conffile.close()
+
+# Вводим основные переменные
 vers = config['core']['version']
 locale = config['settings']['locale']
-
+# Локализация
 gettext.translation('CarMusicSorter', localedir='l10n', languages=[locale]).install()
 
+# Рисуем окно
 window = Tk()
 window.iconphoto(True, PhotoImage(file = 'data/assets/imgs/main.png'))
 window.geometry('400x200')
 window.title(f'Car Music Sorter v.: {vers}')
 
+# Основное меню
+menu = Menu(window)
+menu_about = Menu(menu, tearoff = 0)
+menu.add_cascade(label = _('Info'), menu = menu_about)
+
+# Элементы меню
+menu_about.add_command(label=_('About'), command = lambda: func.popup_about(vers))
+
+window.config(menu = menu)
+
+# Строим элеметны основного окна
 main_label = Label(window, text = _('Choose directory where tracks were taken from')).grid(column = 0, row = 0)
-go_button = Button(window, text = _('Begin!'), command = func.on_click('go')).grid(column = 1, row = 0)
+
+# Главная кнопка ;)
+go_button = Button(window, text = _('Begin!'), command = lambda: func.on_click('go')).grid(column = 1, row = 0)
 
 window.mainloop()
--- functions.py
+++
@@ -1,3 +0,0 @@
-def on_click(cmd):
-    if cmd == 'go':
-        text = 'test'
