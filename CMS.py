712682a7523c83198d1ad78240d3d8eeb13bdffb
intervisionlord <ads-net@mail.ru>
2021-12-09 07:47:48 +0300

    [0.3.3] Оперирование треками
     * Рабочий функционал перемещения треков
     * Рабочий функционал удаления ремиксов и лайвов
     * Рабочий функционал удаления номеров треков из начала имени файла

--- CMS.py
+++ CMS.py
@@ -101,9 +101,10 @@ image = sourceicon, width = 140, height = 20, compound = 'left').grid(column = 1
 dest_button = Button(window, text = _('Choose output DIR'), command = lambda: workdirs('outdir'), \
 image = desticon, width = 140, height = 20, compound = 'left').grid(column = 1, row = 1)
 window.mainloop()
-#################
-exit() # Заглушка
-#################
+
+# Код ниже работает после закрытия окна программы (временно)
+# Для простоты тестировния. Его надо распихать по функциям и прикрепить к кнопке.
+
 # 3. !! Оперирование файлами !!
 if input_dir == '' or output_dir == '':
     print('Не указана начальная или конечная директория')
@@ -118,29 +119,38 @@ except:
 else:
     for path, subdirs, files in os.walk(input_dir):
         for file in files:
+# Перегоняем все MP3 в целевую директорию, потом разберемся что с ними делать
+# Хотя, лучше искать только нужные (отбрасывать лайвы и ремиксы и перегонять уже без них)
             filtered = re.search('.*mp3', file)
             if filtered != None:
                 print(f'{path}/{filtered.group(0)}')
                 shutil.copyfile(f'{path}/{filtered.group(0)}', f'{output_dir}/{filtered.group(0)}')
+
 source_file = []
+# 3.1. Удаление ремиксов и лайвов
 for files in os.walk(output_dir):
     for file in files[2]:
         try:
-            source_file.append(re.search('^\d{1,2}([\-\.]|[\s\-]|[\-\s]|[\.\s]).*', file).group(0))
+            source_file.append(re.search('.*\(.*[Rr]emix.*\).*|.*\(.*[Ll]ive.*\).*', file).group(0))
         except:
-            print('Мусора в начале файла не найдено')
-        try:
-            source_file.append(re.search('[rR]emix|[lL]ive', file).group(0))
-        except:
-            print('лайвы и едиты не найдены')
-        finally:
-            print(source_file)
+            pass
 for file in source_file:
-    new_file = re.split('^\d{1,2}([\-\.]|[\s\-]|[\-\s]|[\.\s])\s?', file)
-    print(new_file[2])
-    shutil.move(f'{output_dir}/{file}', f'{output_dir}/{new_file[2]}')
-# postprocess
-source_file.clean()
-for file in os.walk(output_dir):
-    print(re.search('.*(\(live\))'), file)
-    #source_file.append()
+    print('Removing Remix: ', file)
+    os.remove(f'{output_dir}/{file}')
+source_file.clear() # Очищаем список
+
+# 3.2. Готовим список свежепринесенных файлов с вычищенными ремиксами и лайвами
+for files in os.walk(output_dir):
+    for file in files[2]:
+        try:
+            source_file.append(file)
+        except:
+            pass
+
+# 3.3. Убираем из имен файлов мусор (номера треков в различном формате)
+for file in source_file:
+    new_file = re.sub('^[\d{1,2}\s\-\.]*', '', file)
+#    print(new_file) # Вывод для дебага
+#    print('OldFile: ', file)
+    shutil.move(f'{output_dir}/{file}', f'{output_dir}/{new_file}')
+source_file.clear()
